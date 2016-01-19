639313b0f203bc853ba12be4753da7e81f5da93c
Alexander Wilden <alexander.wilden@aixigo.de>
2016-01-19 16:40:24 +0100

    (#132) Using laxar-jasmine-runner for executing jasmine tests.

--- system.config.js
+++ system.config.js
@@ -1,12 +1,13 @@
 System.config({
-  baseURL: "/",
+  baseURL: "./",
   defaultJSExtensions: true,
   transpiler: "babel",
   babelOptions: {
     "optional": [
       "runtime",
       "optimisation.modules.system"
-    ]
+    ],
+    "retainLines": true
   },
   paths: {
     "github:*": "jspm_packages/github/*",
@@ -14,12 +15,29 @@ System.config({
   },
 
   map: {
+    "angular": "npm:angular@1.4.8",
+    "angular-route": "npm:angular-route@1.4.8",
+    "angular-sanitize": "npm:angular-sanitize@1.4.8",
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "bootstrap-sass-official": "github:twbs/bootstrap-sass@3.3.6",
     "core-js": "npm:core-js@1.2.6",
     "font-awesome": "npm:font-awesome@4.4.0",
+    "laxar": "github:LaxarJS/laxar@2.0.0-pre.246",
+    "laxar-jasmine-runner": "github:LaxarJS/laxar-jasmine-runner@0.1.1",
     "moment": "npm:moment@2.11.1",
+    "github:LaxarJS/laxar-jasmine-runner@0.1.1": {
+      "css": "github:systemjs/plugin-css@0.1.20",
+      "image": "github:systemjs/plugin-image@0.1.0",
+      "jasmine-core": "npm:jasmine-core@2.4.1"
+    },
+    "github:LaxarJS/laxar@2.0.0-pre.246": {
+      "angular": "npm:angular@1.4.8",
+      "angular-route": "npm:angular-route@1.4.8",
+      "angular-sanitize": "npm:angular-sanitize@1.4.8",
+      "jjv": "npm:jjv@1.0.2",
+      "jjve": "npm:jjve@0.5.1"
+    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -31,6 +49,9 @@ System.config({
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
+    },
+    "npm:angular@1.4.8": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
@@ -49,6 +70,14 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:jasmine-core@2.4.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:jjv@1.0.2": {
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:moment@2.11.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
