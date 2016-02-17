f0b8e35126724cbd1998739a7711c5388235d94a
Alexander Wilden <alexander.wilden@aixigo.de>
2016-02-17 12:43:34 +0100

    Fixed relative imports.

--- laxar-uikit.js
+++ laxar-uikit.js
@@ -3,10 +3,10 @@
  * Released under the MIT license.
  * http://laxarjs.org/license
  */
-import * as dom from 'lib/dom';
-import * as formatter from 'lib/formatter';
-import * as i18n from 'lib/i18n';
-import * as parser from 'lib/parser';
+import * as dom from './lib/dom';
+import * as formatter from './lib/formatter';
+import * as i18n from './lib/i18n';
+import * as parser from './lib/parser';
 
 export {
    dom,
